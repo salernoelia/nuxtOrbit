729a6843e87d080fcd80d48feab38dc577b24ce6
salernoelia <putopet@gmail.com>
2023-11-27 00:30:15 +0100

    stuff

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>hi</h1>
+    <!-- <three-scene /> -->
+    <BoxGraph></BoxGraph>
+  </div>
+</template>
+
+<script>
+import MyThreeScene from '~/components/threeScene.vue';
+
+export default {
+  components: {
+    MyThreeScene,
+  },
+};
+
+</script>
